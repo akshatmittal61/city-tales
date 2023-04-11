5641a7d81d0b58cbedb572713edeb807a76e0d34
Akshat Mittal <akshatmittal2506@gmail.com>
2023-04-11 10:17:18 +0530

    config: configured jest for unit tests

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+		],
+		dangerouslyAllowAllLocals: true,
+		dangerouslyAllowHttpProtocols: true,
+		dangerouslyAllowStaticImages: true,
+		dangerouslyAllowSVG: true,
+		domains: ["localhost"],
+	},
 	reactStrictMode: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, "styles")],
+	},
 };
 
 module.exports = nextConfig;
