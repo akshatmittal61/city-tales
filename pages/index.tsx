17f585d5d093e522b6e8751c170c667904f6ef09
Akshat Mittal <akshatmittal2506@gmail.com>
2023-04-11 10:41:54 +0530

    test: testing branch rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 const HomePage: React.FC = () => {
-	return (
-		<div>
-			<h1>Home Page</h1>
-			<p>Some content</p>
-		</div>
-	);
+	return <div></div>;
 };
 
 export default HomePage;
