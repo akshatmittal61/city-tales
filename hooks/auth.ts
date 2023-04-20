541d30b4656b8b8519ffc4f6f9d7a90a19d11a2f
Akshat Mittal <akshatmittal2506@gmail.com>
2023-04-21 03:56:51 +0530

    feat: created custom hook for auth state

---
+++ hooks/auth.ts
@@ -0,0 +1,49 @@
+import { useContext, useEffect, useState } from "react";
+import GlobalContext from "@/context/GlobalContext";
+import { fetchAuthenticatedUser } from "@/utils/api/auth";
+
+const useAuth = () => {
+	const { user: ContextUser } = useContext(GlobalContext);
+	const [user, setUser] = useState<any>(ContextUser);
+	const [loggedIn, setLoggedIn] = useState<boolean>(false);
+	const [loading, setLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		(async function () {
+			try {
+				setLoading(true);
+				if (localStorage.getItem("token")) {
+					if (ContextUser) {
+						setUser(ContextUser);
+						setLoggedIn(true);
+						setLoading(false);
+					} else {
+						const fetchedUser = await fetchAuthenticatedUser();
+						setUser(fetchedUser);
+						setLoggedIn(true);
+						setLoading(false);
+					}
+				} else {
+					setUser(null);
+					setLoggedIn(false);
+					setLoading(false);
+				}
+			} catch (error) {
+				console.error(error);
+				setUser(null);
+				setLoggedIn(false);
+				setLoading(false);
+			} finally {
+				setLoading(false);
+			}
+		})();
+	}, [ContextUser]);
+
+	return {
+		user,
+		loggedIn,
+		loading,
+	};
+};
+
+export default useAuth;
